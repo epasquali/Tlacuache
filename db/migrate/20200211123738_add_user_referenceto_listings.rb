2139203ec774df458939a31e05df0c011bbfc3a0
Elisa Pasquali <epasquali@hotmail.com>
2020-02-13 11:48:51 +0100

    Fixed last migration for heroku maybe

--- db/migrate/20200211123738_add_user_referenceto_listings.rb
+++ db/migrate/20200211123738_add_user_referenceto_listings.rb
@@ -1,6 +1,6 @@
 class AddUserReferencetoListings < ActiveRecord::Migration[6.0]
   def change
     add_column :listings, :user_id,  :integer
+    add_index :listings, [:user_id, :created_at]
   end
-  add_index :listings, [:user_id, :created_at]
 end
