26bb2737b60e17c3064e9e3764b9e7c6d18b2c7d
Elisa Pasquali <epasquali@hotmail.com>
2020-02-14 09:07:46 +0100

    Minor fixing on confirmations views and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
 
     before_action :set_locale
 
+    #Devise redirect route overrides
     def after_sign_in_path_for(resource)
       stored_location_for(resource) || user_path(resource)
     end
 
+    def after_confirmation_path_for(resource)
+      sign_in(resource)
+      stored_location_for(resource) || user_path(resource)
+    end
+
+    #Localization
     def default_url_options
       { locale: I18n.locale }
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   scope "(:locale)", :locale => /#{I18n.available_locales.join("|")}/ do  
 
 
-    devise_for :users, skip: [:sessions, :registrations, :passwords]
+    #devise_for :users, skip: [:sessions]
+    devise_for :users, skip: [:sessions, :registrations, :passwords], controllers: {confirmations: 'users/confirmations'}
     #devise_for :users, skip: [:sessions, :registrations]
   
     #devise_for :users,
@@ -17,27 +18,32 @@ Rails.application.routes.draw do
     #  :unlock => 'unlock',
     #  :registration => 'registration',
     #  :sign_up => 'register' }
-
+ 
 
     devise_scope :user do
+      
       # sessions
       get    '/login',  to: 'devise/sessions#new',     as: :new_user_session
       post   '/login',  to: 'devise/sessions#create',  as: :user_session
       delete '/logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+      
       # registrations
-      put    '/edituser',  to: 'devise/registrations#update'
-      delete '/edituser',  to: 'devise/registrations#destroy'
-      post   '/edituser',  to: 'devise/registrations#create'
+      put    '/users',  to: 'devise/registrations#update'
+      delete '/users',  to: 'devise/registrations#destroy'
+      post   '/users',  to: 'devise/registrations#create'
       get    '/register', to: 'devise/registrations#new',    as: :new_user_registration
-      get    '/edituser',  to: 'devise/registrations#edit',   as: :edit_user_registration
-      patch  '/edituser',  to: 'devise/registrations#update', as: :user_registration
-      get    '/edituser/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration 
+      get    '/users/edit',  to: 'devise/registrations#edit',   as: :edit_user_registration
+      patch  '/users',  to: 'devise/registrations#update', as: :user_registration
+      get    '/users/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration 
   
       # passwords
       get   'forgot-password',  to: 'devise/passwords#new',    as: :new_user_password
-      get   'change-password', to: 'devise/passwords#edit',   as: :edit_user_password
-      patch 'edit-password', to: 'devise/passwords#update'
-      post  'new-password',  to: 'devise/passwords#create', as: :user_password
+      get   'users/password/edit', to: 'devise/passwords#edit',   as: :edit_user_password
+      patch 'users/password', to: 'devise/passwords#update'
+      put 'users/password', to: 'devise/passwords#update'
+      post  'users/password',  to: 'devise/passwords#create', as: :user_password
+    
+    
     end
 
 
