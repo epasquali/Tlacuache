12c3592b6c8121f32a8b21bca64c7c0f5c557f7f
Elisa Pasquali <epasquali@hotmail.com>
2020-02-24 15:33:14 +0100

    Basic mobile responsiveness, fixed index and listings show pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
           info: 'alert-info text-center'
         }[flash_type.to_sym] || flash_type.to_s
       end
+
+      def mobile_device
+        agent = request.user_agent
+        return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
+        return "mobile" if agent =~ /Mobile/
+        return "desktop"
+      end
 end
